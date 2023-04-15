c65ae98345b22833fc3d79c719c7d321a37fe6bb
Western8 <westernchrome@gmail.com>
2023-04-15 10:43:43 +0300

    feat: add method remove

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -86,8 +86,16 @@ class BinarySearchTree {
     if (data === el.data) {
       if (el.left !== null) {
         if (el.right !== null) {
-          let maxRight = el.left.max();
-          maxRight.right = el.right;
+
+          let elLeft = el.left;
+          let maxRight = null;
+          while (elLeft !== null) {
+            maxRight = elLeft;
+            elLeft = elLeft.right;
+          }
+          if (maxRight) {
+            maxRight.right = el.right;
+          }
         }
         this.top = el.left;
       } else {
@@ -96,15 +104,52 @@ class BinarySearchTree {
       return;
     }
 
-    /*
+    let parent = {'node': null, 'left': null};
     while (el !== null) {
+
       if (data === el.data) {
-        el = null;
-        return ;
+
+        let newNode = null;
+
+        if (el.left !== null) {
+          if (el.right !== null) {
+
+            let elLeft = el.left;
+            let maxRight = null;
+            while (elLeft !== null) {
+              maxRight = elLeft;
+              elLeft = elLeft.right;
+            }
+            if (maxRight) {
+              maxRight.right = el.right;
+            }
+          }
+          newNode = el.left;
+        } else {
+          newNode = el.right;
+        }
+
+        if (parent.left) {
+          parent.node.left = newNode;
+        } else {
+          parent.node.right = newNode;
+        }
+        
+        return;
       }
-      el = (data > el.data) ? el.right : el.left;
+
+      if (data > el.data) {
+        parent.node = el;
+        parent.left = false;
+        el = el.right;
+      } else {
+        parent.node = el;
+        parent.left = true;
+        el = el.left;
+      }
+      
     }
-    */
+    
   }
 
   min() {
@@ -147,3 +192,26 @@ console.log('tree ', tree);
 res = tree.root();//.data;
 console.log('res ', res);
 */
+
+const tree = new BinarySearchTree();
+tree.add(9);
+tree.add(14);
+tree.add(2);
+tree.add(6);
+tree.add(128);
+tree.add(8);
+tree.add(31);
+tree.add(54);
+tree.add(1);
+tree.remove(14);
+tree.remove(8);
+tree.remove(9);
+console.log('14 ', tree.has(14));
+console.log('8 ', tree.has(8));
+console.log('9 ', tree.has(9));
+console.log('2 ', tree.has(2));
+console.log('6 ', tree.has(6));
+console.log('128 ', tree.has(128));
+console.log('31 ', tree.has(31));
+console.log('54 ', tree.has(54));
+console.log('1 ', tree.has(1));
